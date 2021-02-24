058ee6dff6bb6335ece9ebc9117728a668232ebd
Крпачовска Марија <krpacovskamarija@gmail.com>
2021-02-25 00:34:53 +0100

    Facebook login added

---
+++ src/main/java/mk/finki/ukim/milenichinja/Service/Impl/FacebookConnectionSignup.java
@@ -0,0 +1,27 @@
+package mk.finki.ukim.milenichinja.Service.Impl;
+
+import mk.finki.ukim.milenichinja.Models.AppUser;
+import mk.finki.ukim.milenichinja.Repository.Jpa.AppUserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.social.connect.Connection;
+import org.springframework.social.connect.ConnectionSignUp;
+import org.springframework.stereotype.Service;
+
+import static org.apache.commons.lang3.RandomStringUtils.randomAlphabetic;
+
+@Service
+public class FacebookConnectionSignup implements ConnectionSignUp {
+
+    @Autowired
+    private AppUserRepository userRepository;
+
+    @Override
+    public String execute(Connection<?> connection) {
+        AppUser user = new AppUser();
+        user.setUsername(connection.getDisplayName());
+        user.setPassword(randomAlphabetic(8));
+        userRepository.save(user);
+        return user.getUsername();
+    }
+
+}
--- src/main/java/mk/finki/ukim/milenichinja/config/WebSecurityConfig.java
+++ src/main/java/mk/finki/ukim/milenichinja/config/WebSecurityConfig.java
@@ -1,7 +1,11 @@
 package mk.finki.ukim.milenichinja.config;
 
 
+import mk.finki.ukim.milenichinja.FacebookLogin.FacebookSignInAdapter;
+import mk.finki.ukim.milenichinja.Service.Impl.FacebookConnectionSignup;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
@@ -9,6 +13,12 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.social.connect.ConnectionFactoryLocator;
+import org.springframework.social.connect.UsersConnectionRepository;
+import org.springframework.social.connect.mem.InMemoryUsersConnectionRepository;
+import org.springframework.social.connect.support.ConnectionFactoryRegistry;
+import org.springframework.social.connect.web.ProviderSignInController;
+import org.springframework.social.facebook.connect.FacebookConnectionFactory;
 
 @Configuration
 @EnableWebSecurity
@@ -23,6 +33,15 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         this.passwordEncoder = passwordEncoder;
         this.authenticationProvider = authenticationProvider;
     }
+
+    @Autowired
+    private FacebookConnectionSignup facebookConnectionSignup;
+
+    @Value("${spring.social.facebook.appSecret}")
+    String appSecret;
+
+    @Value("${spring.social.facebook.appId}")
+    String appId;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -48,6 +67,29 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                 .exceptionHandling().accessDeniedPage("/home/access_denied");
 
+    }
+
+    @Bean
+    public ProviderSignInController providerSignInController() {
+        ConnectionFactoryLocator connectionFactoryLocator =
+                connectionFactoryLocator();
+        UsersConnectionRepository usersConnectionRepository =
+                getUsersConnectionRepository(connectionFactoryLocator);
+        ((InMemoryUsersConnectionRepository) usersConnectionRepository)
+                .setConnectionSignUp(facebookConnectionSignup);
+        return new ProviderSignInController(connectionFactoryLocator,
+                usersConnectionRepository, new FacebookSignInAdapter());
+    }
+
+    private ConnectionFactoryLocator connectionFactoryLocator() {
+        ConnectionFactoryRegistry registry = new ConnectionFactoryRegistry();
+        registry.addConnectionFactory(new FacebookConnectionFactory(appId, appSecret));
+        return registry;
+    }
+
+    private UsersConnectionRepository getUsersConnectionRepository(ConnectionFactoryLocator
+                                                                           connectionFactoryLocator) {
+        return new InMemoryUsersConnectionRepository(connectionFactoryLocator);
     }
 
     @Override
